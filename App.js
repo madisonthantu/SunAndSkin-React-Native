a987eddf8e390eafb4c1a6b1b488b6c02b78cc7b
Madison Thantu <madisonthantu@gmail.com>
2024-04-17 13:17:53 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StyleSheet, Image, Text, View} from 'react-native';
+import { useFonts } from 'expo-font';
 
-export default function App() {
+
+// /* 
+export const DisplayLogo = () => {
+  return (
+    <View style={styles.imageContainer}>
+      <Image
+        style={styles.tinyLogo}
+        source={require('./assets/images/logo/sun.png')}
+      />
+      <Image
+        style={styles.logo}
+        source={require('./assets/images/logo/skincare.png')}
+      />
+    </View>
+  );
+};
+// */
+const currDate = new Date();
+const currDateStr = `${currDate.getDay()}, ${currDate.getMonth()} ${currDate.getDate()}, ${currDate.getFullYear()}`;
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    'Pacifico': require('./assets/fonts/Pacifico.ttf'),
+  });
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> {currDateStr} </Text>
+      <DisplayLogo />
+      <Text style={styles.title}>Sun & Skin</Text>
     </View>
   );
 }
@@ -13,8 +38,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    backgroundColor: '#ff2e55',
+    justifyContent: 'center', alignItems: 'center'
+  },
+  imageContainer: {
+    paddingTop: 25,
+  },
+  title: {
+    marginTop: 0,
+    paddingVertical: 8,
+    color: 'white',
+    backgroundColor: 'rgba(0, 0, 0, 0.0)',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 35,
+    fontWeight: 'bold',
+    fontFamily: 'Pacifico'
+  },
+  tinyLogo: {
+    width: 60,
+    height: 60,
+  },
+  logo: {
+    width: 66,
+    height: 75,
   },
 });
+
+export default App;
